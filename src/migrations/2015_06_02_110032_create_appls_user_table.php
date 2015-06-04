200c20b36c91f8869e0ac03c59693455360e704d
Javito <reivaj_86@hotmail.com>
2015-06-04 19:02:19 +0200

    Update and rename 2015_06_02_110032_create_users_appls_table.php to 2015_06_02_110032_create_appls_user_table.php

--- src/migrations/2015_06_02_110032_create_appls_user_table.php
+++ src/migrations/2015_06_02_110032_create_appls_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersApplsTable extends Migration
+class CreateApplsUserTable extends Migration
 {
 	/**
 	 * Run the migrations.
